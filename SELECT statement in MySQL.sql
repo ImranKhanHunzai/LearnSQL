28b9a1cafc8633df29a12982b7e10768d1c59e54
Imran Khan Hunzai <imran.ash54@gmail.com>
2026-02-10 08:57:37 +0500

    Add files via upload
    SELECT statement in MySQL

---
+++ SELECT statement in MySQL.sql
@@ -0,0 +1,8 @@
+# SELECT ALL DATA
+select * from employee_demographics;
+
+#SELECT SELECTED COLUMNS
+select first_name, last_name from employee_demographics;
+
+#SELECT UNIQUE ROWS
+select distinct gender from employee_demographics
